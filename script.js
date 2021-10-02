82fed012c89bc5025d0315263e04f2416770bca6
MxKlim <86914815+MxKlim@users.noreply.github.com>
2021-10-02 04:07:22 +0300

    Update script.js
    fix: slider add transition js

--- script.js
+++ script.js
@@ -19,17 +19,30 @@ button.forEach(item => {
 })
 
 function moveLastChild (dir) {
-  
 if(dir)  {
-   const newElem = wraper.children[0];
-   wraper.append(newElem);
+  const newElem = wraper.children[0];
+  wraper.style.left = '-200px'; // смещаем врапер на ширину слайда
+    // по окончанию собития transitin отключаем transition, что бы не было заметно смещение left 0
+    // перекидываем элемент в конец родителя
+    wraper.addEventListener("transitionend", ()=>{ 
+      wraper.style.transition ='none';
+      wraper.style.left ='-100px';
+      wraper.append(newElem);
+    });
+    // включаем transition
+    wraper.style.transition ='left .5s'
 }else{
-
   const newElem = wraper.children[count-1];
-   wraper.prepend(newElem);
+  // смещаем слайдер в начальное значение т.к по css указано -100px
+      wraper.style.left = '0px';
+   wraper.addEventListener("transitionend", ()=>{ 
+    //отключаем transition что бы вернуть начальную позицию котора была в css
+      wraper.style.transition ='none';
+       wraper.prepend(newElem);
+       wraper.style.left = '-100px';
+      
+    });
+   wraper.style.transition ='left .5s';
+      
+} 
 }
- 
-  
-}
-
-
